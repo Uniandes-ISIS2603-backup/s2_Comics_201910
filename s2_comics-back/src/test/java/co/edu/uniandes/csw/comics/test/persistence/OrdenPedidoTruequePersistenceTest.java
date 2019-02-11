b5fbabd7931c3963357c1c995d4feb4e956551d7
jenifer rodriguez <jp.rodriguezv@uniandes.edu.co>
2019-02-11 09:25:29 -0500

    clase

---
+++ s2_comics-back/src/test/java/co/edu/uniandes/csw/comics/test/persistence/OrdenPedidoTruequePersistenceTest.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.comics.test.persistence;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Test;
+
+/**
+ *
+ * @author estudiante
+ */
+public class OrdenPedidoTruequePersistenceTest {
+   /**
+     * @return Devuelve el jar que Arquillian va a desplegar en Payara embebido.
+     * El jar contiene las clases, el descriptor de la base de datos y el
+     * archivo beans.xml para resolver la inyección de dependencias.
+     */
+    @Deployment
+    public static JavaArchive createDeployment() {
+        return ShrinkWrap.create(JavaArchive.class)
+                .addPackage(OrdenPedidoTruequeEntity.class.getPackage())
+                .addPackage(OrdenPedidoTruequePersistence.class.getPackage())
+                .addAsManifestResource("META-INF/persistence.xml", "persistence.xml")
+                .addAsManifestResource("META-INF/beans.xml", "beans.xml");
+    }
+    
+   @Test 
+    public void CreatTest(){
+        
+    }
+}
