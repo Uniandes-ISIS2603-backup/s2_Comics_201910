50ff67b041826bb1f5d800942f8a34507a407e31
jenifer rodriguez <jp.rodriguezv@uniandes.edu.co>
2019-02-13 16:17:45 -0500

    dtos

--- s2_comics-api/src/main/java/co/edu/uniandes/csw/comics/resources/OrdenPedidoTruequeResource.java
+++ s2_comics-api/src/main/java/co/edu/uniandes/csw/comics/resources/OrdenPedidoTruequeResource.java
@@ -5,9 +5,17 @@
  */
 package co.edu.uniandes.csw.comics.resources;
 
+import co.edu.uniandes.csw.comics.dtos.OrdenPedidoTruequeDTO;
+import java.util.HashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.enterprise.context.RequestScoped;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 
 /**
@@ -20,5 +28,19 @@ import javax.ws.rs.Produces;
 @Consumes("application/json")
 @RequestScoped
 public class OrdenPedidoTruequeResource {
+   private final static Logger LOGGER = Logger.getLogger(OrdenPedidoTruequeDTO.class.getName());
     
+    /**
+     * Crea un nueva OrdenPedidoTrueque con la informacion que se recibe en el cuerpo de la
+     * petición y se regresa un objeto identico con un id auto-generado por la
+     * base de datos.
+     *
+     * @param ordenPedidoTrueque 
+     * @return JSON {@link BookDTO} - la ordenPedidoTrueque guardada con el atributo id
+     * autogenerado.
+     */
+    @POST
+    public OrdenPedidoTruequeDTO createOrdenPedidoTrueque(OrdenPedidoTruequeDTO ordenPedidoTrueque)  {;
+        return ordenPedidoTrueque;
+    }
 }
