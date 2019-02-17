7c03749e9e92a4448f1027d37ccc69028669acef
Carlos <ca.orduz@uniandes.edu.co>
2019-02-16 21:35:42 -0500

    Merge origin/master 	calificacion persistencia

---
+++ s2_comics-back/src/main/java/co/edu/uniandes/csw/comics/entities/CalificacionEntity.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.comics.entities;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import uk.co.jemos.podam.common.PodamExclude;
+
+/**
+ *
+ * @author estudiante
+ */
+@Entity
+public class CalificacionEntity extends BaseEntity implements Serializable {
+    private String comentarios;
+    private Double puntuacion;
+    @PodamExclude
+    @ManyToOne
+    private VendedorEntity vendedor;
+    public CalificacionEntity(){
+        
+    }
+    /**
+     * @return the comentarios
+     */
+    public String getComentarios() {
+        return comentarios;
+    }
+
+    /**
+     * @param comentarios the comentarios to set
+     */
+    public void setComentarios(String comentarios) {
+        this.comentarios = comentarios;
+    }
+
+    /**
+     * @return the puntuacion
+     */
+    public Double getPuntuacion() {
+        return puntuacion;
+    }
+
+    /**
+     * @param puntuacion the puntuacion to set
+     */
+    public void setPuntuacion(Double puntuacion) {
+        this.puntuacion = puntuacion;
+    }
+
+    /**
+     * @return the vendedor
+     */
+    public VendedorEntity getVendedor() {
+        return vendedor;
+    }
+
+    /**
+     * @param vendedor the vendedor to set
+     */
+    public void setVendedor(VendedorEntity vendedor) {
+        this.vendedor = vendedor;
+    }
+}
