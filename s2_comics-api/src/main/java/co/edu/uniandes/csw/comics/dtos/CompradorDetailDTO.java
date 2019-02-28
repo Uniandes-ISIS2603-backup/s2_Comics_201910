565aef9ceecd5672cd664ccfb338d76453fd579c
Juan Pablo <jp.cano@uniandes.edu.co>
2019-02-27 21:37:18 -0500

    CompradorDetail

--- s2_comics-api/src/main/java/co/edu/uniandes/csw/comics/dtos/CompradorDetailDTO.java
+++ s2_comics-api/src/main/java/co/edu/uniandes/csw/comics/dtos/CompradorDetailDTO.java
@@ -5,13 +5,18 @@
  */
 package co.edu.uniandes.csw.comics.dtos;
 
+import co.edu.uniandes.csw.comics.entities.ComicDeseoEntity;
+import co.edu.uniandes.csw.comics.entities.ComicEntity;
 import co.edu.uniandes.csw.comics.entities.CompradorEntity;
 import co.edu.uniandes.csw.comics.entities.OrdenPedidoEntity;
 import java.util.ArrayList;
+import java.util.List;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
 
 /**
  *
- * @author estudiante
+ * @author juan pablo cano
  */
 public class CompradorDetailDTO extends CompradorDTO
 {
@@ -31,7 +36,105 @@ public class CompradorDetailDTO extends CompradorDTO
         super(entity);
         if(entity != null)
         {
+            pedidos = new ArrayList();
+            for(OrdenPedidoEntity orden : entity.getOrdenPedidoCompra())
+            {
+                pedidos.add(new OrdenPedidoDTO(orden));
+            }
             
+            listaDeseos = new ArrayList();
+            for(ComicDeseoEntity deseo : entity.getListaDeseos())
+            {
+                listaDeseos.add(new ComicDeseoDTO(deseo));
+            }
+            
+            /*carro = new ArrayList();
+            for(ComicEntity comic : entity.getCarro())
+            {
+                carro.add(new ComicDTO(comic));
+            }*/
         }
+    }
+    
+    @Override
+    public CompradorEntity toEntity()
+    {
+        CompradorEntity compradorEntity = super.toEntity();
+        if(getPedidos() != null)
+        {
+            List<OrdenPedidoEntity> pedidosEntity = new ArrayList();
+            for(OrdenPedidoDTO pedido : getPedidos())
+            {
+                pedidosEntity.add(pedido.toEntity());
+            }
+            compradorEntity.setOrdenPedidoCompra(pedidosEntity);
+        }
+        if(getListaDeseos() != null)
+        {
+            List<ComicDeseoEntity> deseos = new ArrayList();
+            for(ComicDeseoDTO deseo : getListaDeseos())
+            {
+                deseos.add(deseo.toEntity());
+            }
+            
+            compradorEntity.setListaDeseos(deseos);
+        }
+        if(getCarro() != null)
+        {
+            List<ComicEntity> comics = new ArrayList();
+            for(ComicDTO comic : getCarro())
+            {
+                //comics.add(comic.to)
+            }
+            compradorEntity.setCarro(comics);
+        }
+        return compradorEntity;
+    }
+
+    /**
+     * @return the pedidos
+     */
+    public ArrayList<OrdenPedidoDTO> getPedidos() {
+        return pedidos;
+    }
+
+    /**
+     * @param pedidos the pedidos to set
+     */
+    public void setPedidos(ArrayList<OrdenPedidoDTO> pedidos) {
+        this.pedidos = pedidos;
+    }
+
+    /**
+     * @return the listaDeseos
+     */
+    public ArrayList<ComicDeseoDTO> getListaDeseos() {
+        return listaDeseos;
+    }
+
+    /**
+     * @param listaDeseos the listaDeseos to set
+     */
+    public void setListaDeseos(ArrayList<ComicDeseoDTO> listaDeseos) {
+        this.listaDeseos = listaDeseos;
+    }
+
+    /**
+     * @return the carro
+     */
+    public ArrayList<ComicDTO> getCarro() {
+        return carro;
+    }
+
+    /**
+     * @param carro the carro to set
+     */
+    public void setCarro(ArrayList<ComicDTO> carro) {
+        this.carro = carro;
+    }
+    
+    public String toString()
+    {
+        return ToStringBuilder.reflectionToString(this, ToStringStyle.MULTI_LINE_STYLE);
     }
 }
