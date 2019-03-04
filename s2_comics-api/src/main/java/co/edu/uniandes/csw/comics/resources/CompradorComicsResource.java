605d742445ac9c397560dc660bdbea978a7554b5
Juan Pablo <jp.cano@uniandes.edu.co>
2019-03-03 22:36:59 -0500

    Resources of Relationships

--- s2_comics-api/src/main/java/co/edu/uniandes/csw/comics/resources/CompradorComicsResource.java
+++ s2_comics-api/src/main/java/co/edu/uniandes/csw/comics/resources/CompradorComicsResource.java
@@ -5,9 +5,11 @@
  */
 package co.edu.uniandes.csw.comics.resources;
 
+import co.edu.uniandes.csw.comics.dtos.ComicDTO;
 import co.edu.uniandes.csw.comics.dtos.CompradorDTO;
 import co.edu.uniandes.csw.comics.dtos.CompradorDetailDTO;
 import co.edu.uniandes.csw.comics.ejb.*;
+import co.edu.uniandes.csw.comics.entities.ComicEntity;
 import co.edu.uniandes.csw.comics.entities.CompradorEntity;
 import co.edu.uniandes.csw.comics.exceptions.BusinessLogicException;
 import java.util.logging.Logger;
@@ -35,4 +37,28 @@ public class CompradorComicsResource
     @Inject 
     private CompradorComicLogic compradorLogic;
     
+    @Inject
+    private ComicLogic comicLogic;
+    
+    private List<ComicEntity> listDto2Entity(List<ComicDTO> comics)
+    {
+        List<ComicEntity> entities = new ArrayList();
+        for(ComicDTO dto : comics)
+        {
+            entities.add(dto.toEntity());
+        }
+        
+        return entities;
+    }
+    
+    private List<ComicDTO> listEntity2DTO(List<ComicEntity> comics)
+    {
+        List<ComicDTO> dtos = new ArrayList();
+        for(ComicEntity entity : comics)
+        {
+            dtos.add(new ComicDTO(entity));
+        }
+        
+        return dtos;
+    }
 }
