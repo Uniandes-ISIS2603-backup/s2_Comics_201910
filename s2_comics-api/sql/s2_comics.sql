e156b9582cf8d6482b33513a937a20be6e90e29b
Carlos <ca.orduz@uniandes.edu.co>
2019-03-28 22:04:52 -0500

    op

--- s2_comics-api/sql/s2_comics.sql
+++ s2_comics-api/sql/s2_comics.sql
@@ -1,5 +1,7 @@
 delete from CompradorEntity;
+delete from VendedorEntity;
 delete from ComicEntity_CompradorEntity;
+delete from ComicEntity_VendedorEntity;
 delete from CompradorEntity_ComicDeseoEntity;
 
 insert into CompradorEntity(id, alias, email, intereses, nombre, foto) values (100,'heavyRock','juan@hotmail.com','Musica','Juan Pedro', 'http://www.esvivir.com/uploads/eres_persona_motivada_711_26121651.jpg');
@@ -11,3 +13,15 @@ insert into CompradorEntity(id, alias, email, intereses, nombre, foto) values (6
 insert into CompradorEntity(id, alias, email, intereses, nombre, foto) values (700,'bebecito','bebecito@hotmail.com','DC Comics','Bebecito','');
 insert into CompradorEntity(id, alias, email, intereses, nombre, foto) values (800,'sherlockHolmes','sherlock@hotmail.com','Terror','Sherlock Holmes','https://upload.wikimedia.org/wikipedia/commons/thumb/0/0d/Statue_of_Sherlock_Holmes_in_Edinburgh.jpg/180px-Statue_of_Sherlock_Holmes_in_Edinburgh.jpg');
 
+insert into VendedorEntity (id, alias, email, intereses, nombre, foto) values (1, 'dcopozio0', 'dcopozio0@illinois.edu', 'Aliquam sit amet diam in magna bibendum imperdiet. Nullam orci pede, venenatis non, sodales sed, tincidunt eu, felis.', 'Dawn Copozio', 'https://robohash.org/praesentiumquisquammaxime.jpg?size=50x50&set=set1');
+insert into VendedorEntity (id, alias, email, intereses, nombre, foto) values (2, 'tdraye1', 'tdraye1@ameblo.jp', 'Suspendisse potenti. In eleifend quam a odio.', 'Teodora Draye', 'https://robohash.org/nihiloditest.bmp?size=50x50&set=set1');
+insert into VendedorEntity (id, alias, email, intereses, nombre, foto) values (3, 'qtimewell2', 'qtimewell2@stanford.edu', 'Suspendisse potenti.', 'Quinn Timewell', 'https://robohash.org/accusantiumquialiquid.jpg?size=50x50&set=set1');
+insert into VendedorEntity (id, alias, email, intereses, nombre, foto) values (4, 'btheodore3', 'btheodore3@amazon.co.jp', 'Vestibulum rutrum rutrum neque.', 'Blane Theodore', 'https://robohash.org/molestiasatquevoluptas.png?size=50x50&set=set1');
+insert into VendedorEntity (id, alias, email, intereses, nombre, foto) values (5, 'mdunkersley4', 'mdunkersley4@bbc.co.uk', 'Quisque erat eros, viverra eget, congue eget, semper rutrum, nulla.', 'Miranda Dunkersley', 'https://robohash.org/quiadeseruntnemo.jpg?size=50x50&set=set1');
+insert into VendedorEntity (id, alias, email, intereses, nombre, foto) values (6, 'gfrostick5', 'gfrostick5@addtoany.com', 'Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Duis faucibus accumsan odio. Curabitur convallis.', 'Gilberta Frostick', 'https://robohash.org/possimusvoluptasvelit.jpg?size=50x50&set=set1');
+insert into VendedorEntity (id, alias, email, intereses, nombre, foto) values (7, 'apounder6', 'apounder6@thetimes.co.uk', 'In sagittis dui vel nisl. Duis ac nibh.', 'Abdul Pounder', 'https://robohash.org/cupiditateinventoremagnam.jpg?size=50x50&set=set1');
+insert into VendedorEntity (id, alias, email, intereses, nombre, foto) values (8, 'saxelby7', 'saxelby7@rediff.com', 'Morbi sem mauris, laoreet ut, rhoncus aliquet, pulvinar sed, nisl. Nunc rhoncus dui vel sem.', 'Sandra Axelby', 'https://robohash.org/eligendiodiodignissimos.bmp?size=50x50&set=set1');
+insert into VendedorEntity (id, alias, email, intereses, nombre, foto) values (9, 'cyakolev8', 'cyakolev8@ning.com', 'Duis ac nibh.', 'Cathrin Yakolev', 'https://robohash.org/quiaipsumveritatis.bmp?size=50x50&set=set1');
+insert into VendedorEntity (id, alias, email, intereses, nombre, foto) values (10, 'dsorton9', 'dsorton9@miitbeian.gov.cn', 'Donec quis orci eget orci vehicula condimentum. Curabitur in libero ut massa volutpat convallis.', 'Devlen Sorton', 'https://robohash.org/eautsuscipit.png?size=50x50&set=set1');
+
+select * from VendedorEntity;
