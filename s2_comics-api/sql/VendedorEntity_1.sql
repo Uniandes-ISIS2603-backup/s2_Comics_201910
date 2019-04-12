ab4f3f6205bdd6a8f8d4b85a7b67077f382e2a83
linkhl <af.hernandezl@uniandes.edu.co>
2019-04-12 15:35:05 -0500

    sql vendedor provicional

---
+++ s2_comics-api/sql/VendedorEntity_1.sql
@@ -0,0 +1,37 @@
+insert into VendedorEntity (id, alias, email, intereses, nombre, foto, password) values (1, 'dpavlovic0', 'dpavlovic0@deliciousdays.com', 'whiteboard rich mindshare', 'Douglass Pavlovic', 'https://robohash.org/utnecessitatibusneque.jpg?size=800x800&set=set1', 'whzx1fXwAvCW');
+insert into VendedorEntity (id, alias, email, intereses, nombre, foto, password) values (2, 'ppaulazzi1', 'ppaulazzi1@umich.edu', 'architect collaborative architectures', 'Pincas Paulazzi', 'https://robohash.org/cupiditatevelaperiam.jpg?size=800x800&set=set1', 'T1XLRt6em');
+insert into VendedorEntity (id, alias, email, intereses, nombre, foto, password) values (3, 'bbattell2', 'bbattell2@imageshack.us', 'evolve best-of-breed synergies', 'Bobbee Battell', 'https://robohash.org/voluptasdignissimoscum.jpg?size=800x800&set=set1', 'Tv3ehnuce');
+insert into VendedorEntity (id, alias, email, intereses, nombre, foto, password) values (4, 'mvedenisov3', 'mvedenisov3@godaddy.com', 'deploy killer paradigms', 'Martina Vedenisov', 'https://robohash.org/autquasvero.jpg?size=800x800&set=set1', 'GyTL0QePhg');
+insert into VendedorEntity (id, alias, email, intereses, nombre, foto, password) values (5, 'svon4', 'svon4@hibu.com', 'evolve mission-critical deliverables', 'Sebastiano Von Oertzen', 'https://robohash.org/debitisveldeleniti.jpg?size=800x800&set=set1', 'O7WytZ3n');
+insert into VendedorEntity (id, alias, email, intereses, nombre, foto, password) values (6, 'radlem5', 'radlem5@booking.com', 'morph bricks-and-clicks infomediaries', 'Rasla Adlem', 'https://robohash.org/cupiditateoptiotenetur.jpg?size=800x800&set=set1', 'FOkiUlYoU');
+insert into VendedorEntity (id, alias, email, intereses, nombre, foto, password) values (7, 'lmatczak6', 'lmatczak6@squidoo.com', 'recontextualize proactive web-readiness', 'Lemmie Matczak', 'https://robohash.org/maximefugasuscipit.jpg?size=800x800&set=set1', 'lRaCguj3');
+insert into VendedorEntity (id, alias, email, intereses, nombre, foto, password) values (8, 'cjanny7', 'cjanny7@cpanel.net', 'leverage turn-key synergies', 'Carolann Janny', 'https://robohash.org/voluptatesdoloremest.jpg?size=800x800&set=set1', 'qwuPLjZufr');
+insert into VendedorEntity (id, alias, email, intereses, nombre, foto, password) values (9, 'ale8', 'ale8@europa.eu', 'streamline 24/365 channels', 'Anita Le Strange', 'https://robohash.org/nobisporroet.jpg?size=800x800&set=set1', 'hAvXo8MnWfe');
+insert into VendedorEntity (id, alias, email, intereses, nombre, foto, password) values (10, 'kjonah9', 'kjonah9@cisco.com', 'maximize sexy ROI', 'Keelby Jonah', 'https://robohash.org/etmolestiaesapiente.jpg?size=800x800&set=set1', 'QPJPuYuNj');
+insert into VendedorEntity (id, alias, email, intereses, nombre, foto, password) values (11, 'gpoaga', 'gpoaga@webnode.com', 'enable robust e-business', 'Gabbey Poag', 'https://robohash.org/estquoet.jpg?size=800x800&set=set1', 'Hk6FvjKfjaum');
+insert into VendedorEntity (id, alias, email, intereses, nombre, foto, password) values (12, 'vmaulkinb', 'vmaulkinb@nps.gov', 'facilitate dynamic paradigms', 'Venita Maulkin', 'https://robohash.org/utearumsit.jpg?size=800x800&set=set1', 'A2PeDda9e');
+insert into VendedorEntity (id, alias, email, intereses, nombre, foto, password) values (13, 'dconsterdinec', 'dconsterdinec@bing.com', 'utilize cutting-edge architectures', 'Dollie Consterdine', 'https://robohash.org/etutaperiam.jpg?size=800x800&set=set1', 'JRJBdQ7');
+insert into VendedorEntity (id, alias, email, intereses, nombre, foto, password) values (14, 'vfairheadd', 'vfairheadd@about.com', 'syndicate frictionless synergies', 'Vanna Fairhead', 'https://robohash.org/sapienteconsecteturmaiores.jpg?size=800x800&set=set1', 'KFhYgNVR');
+insert into VendedorEntity (id, alias, email, intereses, nombre, foto, password) values (15, 'atante', 'atante@typepad.com', 'evolve world-class functionalities', 'Allianora Tant', 'https://robohash.org/quiainqui.jpg?size=800x800&set=set1', 'OHEeTSWQ20q');
+insert into VendedorEntity (id, alias, email, intereses, nombre, foto, password) values (16, 'jnovotnef', 'jnovotnef@gizmodo.com', 'syndicate best-of-breed metrics', 'Johnette Novotne', 'https://robohash.org/voluptateaspernaturea.jpg?size=800x800&set=set1', 'euWFNa');
+insert into VendedorEntity (id, alias, email, intereses, nombre, foto, password) values (17, 'gpercivalg', 'gpercivalg@dailymail.co.uk', 'visualize synergistic action-items', 'Golda Percival', 'https://robohash.org/quodilloqui.jpg?size=800x800&set=set1', 'IW82aL9qc8iI');
+insert into VendedorEntity (id, alias, email, intereses, nombre, foto, password) values (18, 'afrentzh', 'afrentzh@blogs.com', 'streamline revolutionary functionalities', 'Adela Frentz', 'https://robohash.org/ullameaqueveniam.jpg?size=800x800&set=set1', 'JUdkkXca');
+insert into VendedorEntity (id, alias, email, intereses, nombre, foto, password) values (19, 'kwoodusi', 'kwoodusi@unblog.fr', 'matrix value-added schemas', 'Karisa Woodus', 'https://robohash.org/minimavelsed.jpg?size=800x800&set=set1', 'qlpOp8Axcv');
+insert into VendedorEntity (id, alias, email, intereses, nombre, foto, password) values (20, 'srustedgej', 'srustedgej@hud.gov', 'evolve e-business eyeballs', 'Sioux Rustedge', 'https://robohash.org/recusandaevoluptatemofficiis.jpg?size=800x800&set=set1', 'dO2yxnuuOh6a');
+
+
+select st.tablename  from sys.systables st LEFT OUTER join sys.sysschemas ss on (st.schemaid = ss.schemaid) where ss.schemaname ='APP';
+SELECT sc.schemaname, co.constraintname, t.tablename, cg.descriptor, t2.tablename, cg2.descriptor, f.deleterule, f.updaterule
+FROM sys.sysconstraints co
+JOIN sys.sysschemas sc ON co.schemaid = sc.schemaid
+JOIN sys.systables t ON co.tableid = t.tableid
+JOIN sys.sysforeignkeys f ON co.constraintid = f.constraintid
+JOIN sys.sysconglomerates cg ON f.conglomerateid = cg.conglomerateid
+JOIN sys.sysconstraints co2 ON f.keyconstraintid = co2.constraintid
+JOIN sys.systables t2 ON co2.tableid = t2.tableid
+JOIN sys.syskeys k ON co2.constraintid = k.constraintid
+JOIN sys.sysconglomerates cg2 ON k.conglomerateid = cg2.conglomerateid
+WHERE co.type = 'F' 
+    and sc.schemaname = current schema  ;
+
+select * from sys.syscolumns where referenceid = (select tableid from sys.systables where tablename = 'COMPRADORENTITY');
